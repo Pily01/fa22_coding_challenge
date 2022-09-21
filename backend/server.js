ceb35f3381051c49206ebb8e2cc933ab33e602e5
Dominguez <pilymdm@bu.edu>
2022-09-21 13:22:53 -0500

    feat: add mongoose connection and app listen on port 8080

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('corse');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect("mongodb://127.0.0.1:27017/task",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+    
+})
+.then(() => console.log("Connected to DB"))
+.catch(console.error)
+
+app.listen(8080, () => console.log("Server Started on Port 8080"));
