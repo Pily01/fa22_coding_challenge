67c84dd8142af7566a580b77694cf43fd406e336
Dominguez <pilymdm@bu.edu>
2022-09-22 13:22:53 -0500

    feat: add functions with useState, and get Tasks function inuse Effect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,46 @@
+import { useState, useEffect } from 'react';
+
+const API_PORT = "http://localhost:8080";
+
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [popupActive, setPopupActive] = useState(false);
+  const [newTask, setNewTask] = useState("");
+
+  const GetTasks = () => {
+    fetch(API_PORT)
+    .then(res => res.json())
+    .then(data => setTasks(data))
+    .catch(err => console.error("ERROR: ", err))
+  }
+
+  useEffect(() => {
+    GetTasks();
+
+    console.log(tasks);
+  }, []);
+
+
   return (
     <div className="App">
-      <h1> Task App </h1>
+      <h1> Welcome! </h1>
+      <h4> These Are Your Tasks</h4>
+
       <div className="tasks">
           <div className="task">
             <div className="checkbox"></div>
             <div className="text">Get the Bread</div>
-
             <div className="delete-task">x</div>
+          </div>
 
+          <div className="task complete">
+            <div className="checkbox"></div>
+            <div className="text">Get the Bread</div>
+            <div className="delete-task">x</div>
           </div>
       </div>
+
+      
 
 
 
@@ -20,3 +50,4 @@ function App() {
 }
 
 export default App;
+ 
