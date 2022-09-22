45228b953c70734a22e3b81b71d2a44a5689df3b
Dominguez <pilymdm@bu.edu>
2022-09-22 14:00:13 -0500

    feat: add completeTask function and if statement to set the complete class

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,30 +20,33 @@ function App() {
     console.log(tasks);
   }, []);
 
+  const completeTask = async (id) => {
+    const data = await fetch(API_PORT + "/complete/" + id, {method: "PUT"})
+    .then(res => res.json())
+    .catch(err => console.error("ERROR: ", err));
+
+    setTasks(tasks => tasks.map( task => {
+      if(task._id === data._id) {
+        task.complete = data.complete;
+      }
+      return task;
+    }))
+  }
 
   return (
     <div className="App">
       <h1> Welcome! </h1>
       <h4> These Are Your Tasks</h4>
 
-      <div className="tasks">
-          <div className="task">
+      <div className='tasks'>
+        {tasks.map (task => (
+          <div className={"task " + (task.complete ? "complete" : "")} key={task._id} onClick={()=> completeTask(task._id)}>
             <div className="checkbox"></div>
-            <div className="text">Get the Bread</div>
+            <div className="text">{ task.text }</div>
             <div className="delete-task">x</div>
           </div>
-
-          <div className="task complete">
-            <div className="checkbox"></div>
-            <div className="text">Get the Bread</div>
-            <div className="delete-task">x</div>
-          </div>
+        ))}  
       </div>
-
-      
-
-
-
     </div>
     
   );
