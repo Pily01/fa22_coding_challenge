c0cf6976fe973f60616c187d96aaad248f948587
Dominguez <pilymdm@bu.edu>
2022-09-22 12:40:50 -0500

    feat: add first html draft

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,20 @@ function App() {
   return (
     <div className="App">
       <h1> Task App </h1>
+      <div className="tasks">
+          <div className="task">
+            <div className="checkbox"></div>
+            <div className="text">Get the Bread</div>
+
+            <div className="delete-task">x</div>
+
+          </div>
+      </div>
+
+
+
     </div>
+    
   );
 }
 
